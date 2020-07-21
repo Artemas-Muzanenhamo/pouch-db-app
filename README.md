cba3bc94f3ab612db0ae652a455cc2522f62f512
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-07-21 22:31:59 +0100

    Add Node back end

---
+++ README.md
@@ -0,0 +1,17 @@
+# PouchDB app
+
+## Pre-requisites
+
+* NodeJs
+
+## season-app-server
+
+* Through a terminal you can start up the Node server by executing:
+
+```javascript
+node season-app-server.js
+```
+* You should be able to navigate to: 
+```javascript
+localhost:8080
+```
---
+++ season-app-server.js
@@ -0,0 +1,13 @@
+let http = require('http');
+
+//create a server object:
+http.createServer((req, res) => {
+    const words = ["SUMMER", "WINTER", "AUTUMN", "SPRING"];
+
+    let word = words[Math.floor(Math.random() * words.length)];
+    let seasonValue = {season: word};
+    
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(seasonValue)); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
