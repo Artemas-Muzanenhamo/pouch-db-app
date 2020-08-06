a897ce637cd9262dbe904323eebafa8e5a3fab94
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-08-06 21:48:59 +0100

    allow CORS for UI

--- season-app-server/season-app-server.js
+++ season-app-server/season-app-server.js
@@ -6,7 +6,13 @@ http.createServer((req, res) => {
 
     let word = words[Math.floor(Math.random() * words.length)];
     let seasonValue = {season: word};
-    
+
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.write(JSON.stringify(seasonValue)); //write a response to the client
     res.end(); //end the response
