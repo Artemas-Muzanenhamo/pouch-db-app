0c5c114b4ae20f5740d76c73f02b97c3e0013bd8
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-08-09 22:13:54 +0100

    add implementation to add data to pouchDB

--- season-app-ui/src/app/app.service.ts
+++ season-app-ui/src/app/app.service.ts
@@ -3,12 +3,14 @@ import PouchDB from 'pouchdb'
 import {Observable} from "rxjs";
 import {Season} from "./season";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {error} from "util";
 
 @Injectable()
 export class AppService {
   private isInstantiated: boolean;
   private database: any;
   private url = 'http://localhost:8888';
+  private season: Season = new Season('');
 
   public constructor(
     private http: HttpClient
@@ -16,10 +18,30 @@ export class AppService {
     if(!this.isInstantiated) {
       this.database = new PouchDB("artemas-test-db");
       this.isInstantiated = true;
+      console.log('PouchDB database created: ', this.database.name);
     }
   }
 
   public getSeason(): Observable<Season> {
-    return this.http.get<Season>(this.url);
+    let seasonObservable = this.http.get<Season>(this.url);
+    seasonObservable
+      .subscribe(response => this.addDoc(response),
+        error => console.log(error));
+    return seasonObservable;
+  }
+
+  private addDoc(response: Season) {
+    // let documentInDB = this.database.get('season')
+    //   .catch(error => console.log('ERROR: The document is missing: ', error));
+    // if (documentInDB) {
+    //   this.database.destroy(documentInDB);
+    // }
+
+    return this.database.put({
+      _id: 'SEASON',
+      season: response
+    })
+      .then(() => console.log("Document created"))
+      .catch(err => console.log('ERROR: ', err));
   }
 }
