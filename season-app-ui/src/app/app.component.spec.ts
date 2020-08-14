d08d53983a6099d5d85412e88a6563b680952686
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-08-14 01:45:52 +0100

    fix initial tests

--- season-app-ui/src/app/app.component.spec.ts
+++ season-app-ui/src/app/app.component.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {AppService} from "./app.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {PouchDbService} from "./pouch-db.service";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +10,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        AppService,
+        PouchDbService
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
     }).compileComponents();
   }));
 
@@ -16,16 +26,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'season-app-ui'`, () => {
+  it(`should have as title 'RANDOM SEASON OF THE DAY IS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('season-app-ui');
+    expect(app.title).toEqual('RANDOM SEASON OF THE DAY IS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('season-app-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('RANDOM SEASON OF THE DAY IS');
   });
 });
